fa691837c44f7704711b9d64f5dd48ac430896fe
MohamadAlaskari <m_alskari96@hotmail.com>
2023-05-13 22:32:38 +0200

    ceate layout and scss and fix css

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 const root = document.documentElement;
 var show = document.querySelector("#show");
 var range = document.querySelector("#range");
+var container = document.querySelector(".earth_container");
+
+/**
+ * fillEarth 
+ * 
+ * gives the percentage of carcentage of carbon
+ * 
+ */
+const fillEarth = () => {
+    root.style.setProperty('--circle-fill', range.value + "%");
+    show.innerHTML = range.value + "%"
+    //root.style.setProperty('--show-percentage', range.value + "%");
 
 
-range.addEventListener("input",()=>{
-    show.innerHTML=range.value + "%"
+}
 
-    root.style.setProperty('--circle-fill', range.value+"%");
-})
+
+const app = () => {
+    range.addEventListener("input", fillEarth);
+}
+app();
+
 
